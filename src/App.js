737203a901da1f2667d01813362ff86118dcab31
supriya <supriya123pink@gmail.com>
2023-11-16 21:01:39 +0530

    Buttons replaced with slider

--- src/App.js
+++ src/App.js
@@ -12,20 +12,35 @@ export default function App() {
 function Counter() {
   const [count, setCount] = useState(0);
   const [step, setStep] = useState(1);
-  const date = new Date("Nov 14");
+
+  function handleReset() {
+    setCount(0);
+    setStep(1);
+  }
+
+  const date = new Date("Nov 16 2023");
   date.setDate(date.getDate() + count);
   return (
     <div>
       <center>
         <div>
-          <button onClick={() => setStep((s) => s - 1)}>-</button>
-          <span>Step: {step}</span>
-          <button onClick={() => setStep((s) => s + 1)}>+</button>
+          <input
+            type="range"
+            min="0"
+            max="10"
+            value={step}
+            onChange={(e) => setStep(Number(e.target.value))}
+          />
+          <span>{step}</span>
         </div>
 
         <div>
           <button onClick={() => setCount((c) => c - step)}>-</button>
-          <span>Count: {count}</span>
+          <input
+            type="text"
+            value={count}
+            onChange={(e) => setCount(Number(e.target.value))}
+          />
           <button onClick={() => setCount((c) => c + step)}>+</button>
         </div>
 
@@ -39,6 +54,11 @@ function Counter() {
           </span>
           <span>{date.toDateString()}</span>
         </p>
+        {count !== 0 || step !== 1 ? (
+          <div>
+            <button onClick={handleReset}>Reset</button>
+          </div>
+        ) : null}
       </center>
     </div>
   );
